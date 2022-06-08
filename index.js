feaa912808a6579e65b65ca809c33ad221f07934
Wangari-Sharon <wangarik35@gmail.com>
2022-06-08 17:49:05 +0300

    commit tree 07a52185164b7508d014b164396fb2cc3c883bd4

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction(spy) {
+    return spy();
+  }
+  
+  function returnsANamedFunction() {
+    return function name() {};
+  }
+  
+  function returnsAnAnonymousFunction() {
+    return function () {};
+  }
+  
+  
